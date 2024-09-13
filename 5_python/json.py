31ce946562795bc79ae4691a3c0bdf2a1f3f99b3
kushal1997 <kushalrao103@gmail.com>
2024-09-13 11:38:54 +0530

    basci python scripting with json vs yaml

---
+++ 5_python/json.py
@@ -0,0 +1,23 @@
+#dictionary
+print("")
+print("Dictionary.................")
+devops={"skill":"DevOps","Year":2024,"Tech":{
+    "Cloud":"AWS","Containers":"KBs","CICD":"Jenkins"
+} }
+print(devops)
+
+devops={"skill":"DevOps",
+"Year":2024,
+"Tech":{
+    "Cloud":"AWS",
+    "Containers":"KBs",
+    "CICD":"Jenkins"
+},
+"gitOps":[
+    "Gitlab",
+    "Git",
+    "Git bucket"
+    ] 
+    
+}
+print(devops)
---
+++ 5_python/main.py
@@ -0,0 +1,66 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
+print(" ")
+print("I am learning Devops")
+
+print("")
+#hello world
+print("this is a comment")
+
+#string variables
+print("")
+print("strings...............")
+skill="Devops"
+print(skill)
+
+
+#string variables
+print("")
+print("integers...............")
+NUM=123
+print(NUM)
+
+#lists []
+print("")
+print("Lists []..................")
+tools=["jenkins","Docker","KBs","Terraform",90]
+print(tools)
+print(tools[0])
+print(tools[-1])
+print(tools[1:4])
+print(tools[1:4][0])
+
+
+# Tuple []
+print("")
+print("Tuple ()..................")
+tools=("jenkins","Docker","KBs","Terraform",90)
+print(tools)
+
+#dictionary
+print("")
+print("Dictionary.................")
+devops={"skill":"DevOps","Year":2024,"Tech":"","GitOps":""}
+print(devops)
+print(devops["skill"])
+print(devops["skill"][1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
